4d423bb7b91cf460cb37e756cddf741241536a34
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-22 18:07:30 +0200

    add implementation of the method in interface with the dialogboxing message with a beep to notify

--- src/PushNotification.java
+++ src/PushNotification.java
@@ -3,5 +3,10 @@ import java.awt.*;
 
 public class PushNotification implements NotificationService{
     String title = "Notification";
+    @Override
+    public void sendNotification(String message) {;
+        Toolkit.getDefaultToolkit().beep();
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+    }
 
 }
