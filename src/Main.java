058afc6bf1789c070c83867cdfaf478bb48ac722
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-23 11:12:31 +0200

    running them in main class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+
+            @Override
+            public void run() {
+                new PushNotification().sendNotification("New message received!");
+            }
+        };
+        timer.schedule(task ,5000);
+
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Test {
+    public static void main(String[] args) {
+        /*the dialog box showing the message and the title */
+//        JOptionPane.showMessageDialog(null, "Hello World" , "Notification", JOptionPane.PLAIN_MESSAGE);
+
+        /*beeping message in the console */
+//        Toolkit.getDefaultToolkit().beep();
+//        System.out.println("Hello World!");
+        /*combining both */
+        Toolkit.getDefaultToolkit().beep();
+        JOptionPane.showMessageDialog(null, "Hello World" , "Notification", JOptionPane.PLAIN_MESSAGE);git
+
+    }
 }
