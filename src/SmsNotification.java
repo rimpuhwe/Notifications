0a22906782462dcb1933f9733952beb40d247bcb
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-23 13:54:16 +0200

    add the Notification method from the sms

--- src/SmsNotification.java
+++ src/SmsNotification.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.awt.*;
 
-public class SmsNotification {
+public class SmsNotification extends PushNotification{
+     public SmsNotification() {
+         super();
+     }
+    @Override
+    public void sendNotification(String message) {
+         Toolkit.getDefaultToolkit().beep();
+         System.out.println("New SMS Message :" + message);
+    }
 }
